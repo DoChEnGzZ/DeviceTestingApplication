3aa90a812adc1e6e40198200622366f0ff2a0aac
dochengzz <dochengzz@hotmail.com>
2021-04-07 16:20:38 +0800

    建立工程

---
+++ app/src/main/java/com/example/devicetestingapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.devicetestingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import org.apache.commons.net.tftp.TFTPClient;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
